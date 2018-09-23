a701261081235aee178b39766343e44c0a051856
muge <muge.hasilci@hotmail.com>
2018-09-23 21:43:31 +0100

    First test

---
+++ spec/factorial_calculator_spec.rb
@@ -0,0 +1,11 @@
+require "rspec/autorun"
+
+describe Calculator do
+  describe "#add" do
+    it "adds two numbers" do
+      calculator = Calculator.new
+
+      expect(calculator.add(1, 1)).to eq(2)
+    end
+  end
+end
